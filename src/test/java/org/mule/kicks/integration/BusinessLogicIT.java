b2eeda1b2a9f73b96d70d083c387fe18de74744e
Miguel Oliva <moliva@mulesoft.com>
2014-02-06 12:49:44 -0300

    Test added with batch module and polling'

--- src/test/java/org/mule/kicks/integration/BusinessLogicIT.java
+++ src/test/java/org/mule/kicks/integration/BusinessLogicIT.java
@@ -1,6 +1,8 @@
 package org.mule.kicks.integration;
 
+import static junit.framework.Assert.assertTrue;
 import static org.junit.Assert.assertEquals;
+import static org.mule.kicks.builders.SfdcObjectBuilder.anAccount;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -9,40 +11,72 @@ import java.util.Map;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.mule.MessageExchangePattern;
 import org.mule.api.MuleEvent;
 import org.mule.api.MuleException;
+import org.mule.api.context.notification.ServerNotification;
 import org.mule.context.notification.NotificationException;
 import org.mule.kicks.test.utils.ListenerProbe;
 import org.mule.kicks.test.utils.PipelineSynchronizeListener;
 import org.mule.processor.chain.SubflowInterceptingChainLifecycleWrapper;
 import org.mule.tck.probe.PollingProber;
+import org.mule.tck.probe.Probe;
 import org.mule.tck.probe.Prober;
 import org.mule.transport.NullPayload;
 
+import com.mulesoft.module.batch.api.BatchJobInstance;
+import com.mulesoft.module.batch.api.notification.BatchNotification;
+import com.mulesoft.module.batch.api.notification.BatchNotificationListener;
+import com.mulesoft.module.batch.engine.BatchJobInstanceAdapter;
+import com.mulesoft.module.batch.engine.BatchJobInstanceStore;
 import com.sforce.soap.partner.SaveResult;
 
 /**
  * The objective of this class is to validate the correct behavior of the Mule Kick that make calls to external systems.
  * 
- * @author miguel.oliva
  */
-public class AccountsOneWaySyncIT extends AbstractKickTestCase {
+public class BusinessLogicIT extends AbstractKickTestCase {
+
 	private static final String POLL_FLOW_NAME = "triggerFlow";
-	private static final String KICK_NAME = "accountsonewaysync";
+	private static final String KICK_NAME = "sfdc2sfdc-accounts-onewaysync";
+
+	private static final int TIMEOUT = 60;
 
 	private static SubflowInterceptingChainLifecycleWrapper checkAccountflow;
 	private static List<Map<String, Object>> createdAccountsInA = new ArrayList<Map<String, Object>>();
 
-	private final Prober workingPollProber = new PollingProber(10000, 1000l);
-
+	private final Prober pollProber = new PollingProber(10000, 1000);
 	private final PipelineSynchronizeListener pipelineListener = new PipelineSynchronizeListener(POLL_FLOW_NAME);
+
+	private Prober prober;
+	protected Boolean failed;
+	protected BatchJobInstanceStore jobInstanceStore;
+
+	@BeforeClass
+	public static void setTestProperties() {
+		System.setProperty("page.size", "1000");
+
+		// Set the frequency between polls to 10 seconds
+		System.setProperty("polling.frequency", "10000");
+
+		// Set the poll starting delay to 20 seconds
+		System.setProperty("polling.start.delay", "20000");
+
+		// Setting Default Watermark Expression to query SFDC with LastModifiedDate greater than ten seconds before current time
+		System.setProperty("watermark.default.expression", "#[groovy: new Date(System.currentTimeMillis() - 10000).format(\"yyyy-MM-dd'T'HH:mm:ss.SSS'Z'\", TimeZone.getTimeZone('UTC'))]");
+	}
 
 	@Before
 	public void setUp() throws Exception {
 		stopFlowSchedulers(POLL_FLOW_NAME);
 		registerListeners();
+
+		failed = null;
+		jobInstanceStore = muleContext.getRegistry()
+										.lookupObject(BatchJobInstanceStore.class);
+		muleContext.registerListener(new BatchWaitListener());
 
 		// Flow to retrieve accounts from target system after syncing
 		checkAccountflow = getSubFlow("retrieveAccountFlow");
@@ -54,14 +88,23 @@ public class AccountsOneWaySyncIT extends AbstractKickTestCase {
 	@After
 	public void tearDown() throws Exception {
 		stopFlowSchedulers(POLL_FLOW_NAME);
+
+		failed = null;
+
 		deleteEntities();
 	}
 
 	@Test
 	public void testMainFlow() throws Exception {
+		// Run poll and wait for it to run
 		runSchedulersOnce(POLL_FLOW_NAME);
-
 		waitForPollToRun();
+		
+		// Wait for the batch job executed by the poll flow to finish
+		BatchJobInstance batchJobInstance = (BatchJobInstance) pipelineListener.getNotificatedPayload();
+		awaitJobTermination();
+		assertTrue("Batch job was not successful", wasJobSuccessful());
+		batchJobInstance = getUpdatedInstance(batchJobInstance);
 
 		// Assert first object was not sync
 		assertEquals("The account should not have been sync", null, invokeRetrieveAccountFlow(checkAccountflow, createdAccountsInA.get(0)));
@@ -83,14 +126,8 @@ public class AccountsOneWaySyncIT extends AbstractKickTestCase {
 		muleContext.registerListener(pipelineListener);
 	}
 
-	private void resetListeners() {
-		pipelineListener.resetListener();
-	}
-
 	private void waitForPollToRun() {
-		System.out.println("Waiting for poll to run ones...");
-		workingPollProber.check(new ListenerProbe(pipelineListener));
-		System.out.println("Poll flow done");
+		pollProber.check(new ListenerProbe(pipelineListener));
 	}
 
 	@SuppressWarnings("unchecked")
@@ -101,7 +138,6 @@ public class AccountsOneWaySyncIT extends AbstractKickTestCase {
 		final MuleEvent event = flow.process(getTestEvent(accountMap, MessageExchangePattern.REQUEST_RESPONSE));
 		final Object payload = event.getMessage()
 									.getPayload();
-		System.out.println("Retrieve Accounts Result for: " + account.get("Name") + " is " + payload);
 		if (payload instanceof NullPayload) {
 			return null;
 		} else {
@@ -118,9 +154,9 @@ public class AccountsOneWaySyncIT extends AbstractKickTestCase {
 
 		final List<Map<String, Object>> createdAccountInB = new ArrayList<Map<String, Object>>();
 		// This account should BE sync (updated) as the industry is Education, has more than 5000 Employees and the record exists in the target system
-		createdAccountInB.add(anAccount().withProperty("Name", buildUniqueName(KICK_NAME, "DemoUpdateAccount"))
-											.withProperty("Industry", "Education")
-											.withProperty("NumberOfEmployees", 17000)
+		createdAccountInB.add(anAccount().with("Name", buildUniqueName(KICK_NAME, "DemoUpdateAccount"))
+											.with("Industry", "Education")
+											.with("NumberOfEmployees", 17000)
 											.build());
 		createAccountInBFlow.process(getTestEvent(createdAccountInB, MessageExchangePattern.REQUEST_RESPONSE));
 
@@ -129,41 +165,38 @@ public class AccountsOneWaySyncIT extends AbstractKickTestCase {
 		createAccountInAFlow.initialise();
 
 		// This account should not be synced as the industry is not "Education" or "Government"
-		createdAccountsInA.add(anAccount().withProperty("Name", buildUniqueName(KICK_NAME, "DemoFilterIndustryAccount"))
-											.withProperty("Industry", "Insurance")
-											.withProperty("NumberOfEmployees", 17000)
+		createdAccountsInA.add(anAccount().with("Name", buildUniqueName(KICK_NAME, "DemoFilterIndustryAccount"))
+											.with("Industry", "Insurance")
+											.with("NumberOfEmployees", 17000)
 											.build());
 
 		// This account should not be synced as the number of employees is less than 5000
-		createdAccountsInA.add(anAccount().withProperty("Name", buildUniqueName(KICK_NAME, "DemoFilterIndustryAccount"))
-											.withProperty("Industry", "Government")
-											.withProperty("NumberOfEmployees", 2500)
+		createdAccountsInA.add(anAccount().with("Name", buildUniqueName(KICK_NAME, "DemoFilterIndustryAccount"))
+											.with("Industry", "Government")
+											.with("NumberOfEmployees", 2500)
 											.build());
 
 		// This account should BE synced (inserted) as the number of employees if greater than 5000 and the industry is "Government"
-		createdAccountsInA.add(anAccount().withProperty("Name", buildUniqueName(KICK_NAME, "DemoCreateAccount"))
-											.withProperty("Industry", "Government")
-											.withProperty("NumberOfEmployees", 18000)
+		createdAccountsInA.add(anAccount().with("Name", buildUniqueName(KICK_NAME, "DemoCreateAccount"))
+											.with("Industry", "Government")
+											.with("NumberOfEmployees", 18000)
 											.build());
 
 		// This account should BE synced (updated) as the number of employees if greater than 5000 and the industry is "Education"
-		createdAccountsInA.add(anAccount().withProperty("Name", buildUniqueName(KICK_NAME, "DemoUpdateAccount"))
-											.withProperty("Industry", "Education")
-											.withProperty("NumberOfEmployees", 12000)
+		createdAccountsInA.add(anAccount().with("Name", buildUniqueName(KICK_NAME, "DemoUpdateAccount"))
+											.with("Industry", "Education")
+											.with("NumberOfEmployees", 12000)
 											.build());
 
 		final MuleEvent event = createAccountInAFlow.process(getTestEvent(createdAccountsInA, MessageExchangePattern.REQUEST_RESPONSE));
 		final List<SaveResult> results = (List<SaveResult>) event.getMessage()
 																	.getPayload();
-		System.out.println("Results from creation in A" + results.toString());
 		int i = 0;
 		for (SaveResult result : results) {
 			Map<String, Object> accountInA = createdAccountsInA.get(i);
 			accountInA.put("Id", result.getId());
 			i++;
 		}
-
-		System.out.println("Results after adding" + createdAccountsInA.toString());
 	}
 
 	private void deleteEntities() throws MuleException, Exception {
@@ -191,26 +224,46 @@ public class AccountsOneWaySyncIT extends AbstractKickTestCase {
 		deleteAccountFromBflow.process(getTestEvent(idList, MessageExchangePattern.REQUEST_RESPONSE));
 	}
 
-	// ***************************************************************
-	// ======== AccountBuilder class ========
-	// ***************************************************************
-	private AccountBuilder anAccount() {
-		return new AccountBuilder();
+	protected class BatchWaitListener implements BatchNotificationListener {
+
+		public synchronized void onNotification(ServerNotification notification) {
+			final int action = notification.getAction();
+
+			if (action == BatchNotification.JOB_SUCCESSFUL || action == BatchNotification.JOB_STOPPED) {
+				failed = false;
+			} else if (action == BatchNotification.JOB_PROCESS_RECORDS_FAILED || action == BatchNotification.LOAD_PHASE_FAILED || action == BatchNotification.INPUT_PHASE_FAILED
+					|| action == BatchNotification.ON_COMPLETE_FAILED) {
+
+				failed = true;
+			}
+		}
 	}
 
-	private static class AccountBuilder {
-
-		private final Map<String, Object> account = new HashMap<String, Object>();
-
-		public AccountBuilder withProperty(final String key, final Object value) {
-			account.put(key, value);
-			return this;
-		}
-
-		public Map<String, Object> build() {
-			return account;
-		}
-
+	protected void awaitJobTermination() throws Exception {
+		this.awaitJobTermination(TIMEOUT);
 	}
 
+	protected void awaitJobTermination(long timeoutSecs) throws Exception {
+		this.prober = new PollingProber(timeoutSecs * 1000, 500);
+		this.prober.check(new Probe() {
+
+			@Override
+			public boolean isSatisfied() {
+				return failed != null;
+			}
+
+			@Override
+			public String describeFailure() {
+				return "batch job timed out";
+			}
+		});
+	}
+
+	protected boolean wasJobSuccessful() {
+		return this.failed != null ? !this.failed : false;
+	}
+
+	protected BatchJobInstanceAdapter getUpdatedInstance(BatchJobInstance jobInstance) {
+		return this.jobInstanceStore.getJobInstance(jobInstance.getOwnerJobName(), jobInstance.getId());
+	}
 }

94bd893444210d43f609b4425b1ebf0432936461
Marek <marecica2@gmail.com>
2014-06-06 12:32:11 +0200

    test fixes

--- src/test/java/org/mule/templates/integration/BusinessLogicIT.java
+++ src/test/java/org/mule/templates/integration/BusinessLogicIT.java
@@ -69,6 +69,8 @@ public class BusinessLogicIT extends AbstractTemplateTestCase {
 
 		// Assert third object was sync to target system 
 		Map<String, Object> payload = invokeRetrieveFlow(retrieveAccountFromBFlow, createdAccountsInA.get(2));
+		System.err.println(createdAccountsInA.get(2));
+		System.err.println(payload);
 		assertEquals("The account should have been sync", createdAccountsInA.get(2).get("Name"), payload.get("Name"));
 
 		// Assert fourth object was sync to target system
